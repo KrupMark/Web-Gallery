b19d4aaa0ac97fc3085bfcfe01b4390fcec2ce31
KrupMark <krupinczamark@hotmail.com>
2018-01-21 23:24:12 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/login",function(req,res){
+  res.sendFile(path + "login.html");
+});
+
+app.post('/login', function (req, res) {
+  var post = req.body;
+  if (post.User === 'albert' && post.Password === 'valami') {
+    req.session.user_id = albert_user_id_here;
+    res.redirect('/');
+  } else {
+    res.send('Rossz felhasználónév/jelszó');
+  }
+});
+
+router.get("/register",function(req,res){
+  res.sendFile(path + "register.html");
+});
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("3000-es porton elérhető!");
+});
