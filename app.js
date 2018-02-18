766fd491f9d2286a911874c78269cbd8d3948af0
KrupMark <krupinczamark@hotmail.com>
2018-02-19 00:20:05 +0100

    mongodb

--- app.js
+++ app.js
@@ -1,78 +1,43 @@
+// set up ======================================================================
 var express = require('express');
 var app = express();
-var router = express.Router();
-var path = __dirname + '/views/';
-var Mainpage = 'index.html';
+var port     = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-// Login data
-loginUser = 'albert';
-loginPasswd = 'valami';
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-var bodyParser = require('body-parser');
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
+var configDB = require('./config/database.js');
+
+// // configuration ===============================================================
+// mongoose.connect(configDB.url); //connect to database
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'lialialialia', // session secret
+    resave: true,
+    saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// Index or Main = /
-router.get("/",function(req,res) {
-  res.sendFile(path + Mainpage);
-});
-
-// Login = /login
-router.get("/login",function(req,res){
-  res.sendFile(path + "login.html");
-});
-
-// Register = /register
-router.get("/register",function(req,res){
-  res.sendFile(path + "register.html");
-});
-
-// Login
-app.post('/login', function (req, res) {
-  var post = req.body;
-  console.log("post data: "+post.user);
-  console.log(post);
-  if (post.user === loginUser && post.password === loginPasswd) {
-    // req.session.user_id = user_id;
-    Mainpage = 'main.html';
-    res.redirect('/');
-  } else {
-    res.send('Rossz felhasználónév/jelszó');
-  }
-});
-
-// Registration
-app.post('/reg', function (req, res) {
-  var post = req.body;
-  console.log(post);
-  dataUsername = post.user;
-  dataPasswd = post.password;
-  dataEmail = post.email;
-  dataConfEmail = post.confEmail;
-  if (post.email === post.confEmail) {
-    res.send('Sikeres regisztráció!');
-  } else {
-    console.log("E-mail address error!");
-    res.send('Nem egyezik az e-mail cím!');
-  }
-});
-
-// Logout
-router.get("/logout",function(req,res){
-  Mainpage = 'index.html';
-  res.redirect('/');
-});
-
-app.use("/",router);
-
-app.listen(3000,function(){
-  console.log("A szerver működik")
-  console.log("3000-es porton elérhető!");
-});
+// launch ======================================================================
+app.listen(port);
+console.log('Ezen a porton elérhető: ' + port);
