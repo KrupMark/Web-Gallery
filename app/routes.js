844f7e6edfc1ef48507915767ac62fd6ee9bb6f7
KrupMark <krupinczamark@hotmail.com>
2018-03-05 00:13:10 +0100

    registration/login

--- app/routes.js
+++ app/routes.js
@@ -47,35 +47,6 @@ module.exports = function(app, passport) {
             failureRedirect : '/signup', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
-
-// AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT)
-
-    // locally
-        app.get('/connect/local', function(req, res) {
-            res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-        });
-        app.post('/connect/local', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
-
-
-// UNLINK ACCOUNTS
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove username and password
-// user account will stay active in case they want to reconnect in the future
-
-    // local
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.userName    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
-    });
-
 };
 
 // route middleware to ensure user is logged in
