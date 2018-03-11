121feda9b93fcb07577f80259cc7838e72006916
KrupMark <krupinczamark@hotmail.com>
2018-03-12 00:10:41 +0100

    upload

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 3000;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-
+var express      = require('express');
+var app          = express();
+var mongoose     = require('mongoose');
+var passport     = require('passport');
+var flash        = require('connect-flash');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-
-var configDB = require('./config/database.js');
+var configDB     = require('./config/database.js');
+var port         = process.env.PORT || 3000;
+var multer       = require('multer');
+var upload       = multer({dest: 'uploads/'});
+var sizeOf       = require('image-size');
+var exphbs       = require('express-handlebars');
+require('string.prototype.startswith');
 
 // configuration
 mongoose.connect(configDB.url); // connect to our database
@@ -22,6 +25,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/bower_components'));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
@@ -34,6 +38,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
+
+app.engine( '.hbs', exphbs( { extname: '.hbs' } ) );
+app.set('view engine', '.hbs');
 
 // routes
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
--- app/routes.js
+++ app/routes.js
@@ -1,58 +1,85 @@
 module.exports = function(app, passport) {
-
 // page routes
 
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
+  // HOME PAGE
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
+
+  // PROFILE PAGE
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+      user : req.user
     });
+  });
 
-    // PROFILE SECTION
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user
-        });
+  // UPLOAD PAGE
+  app.get('/upload', isLoggedIn, function(req, res) {
+    res.render('upload.ejs', {
+      user : req.user
     });
+  });
 
-    // LOGOUT
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
+  var multer = require('multer');
+  var upload = multer({dest: 'uploads/'});
+  var sizeOf = require('image-size');
+  var exphbs = require('express-handlebars');
 
-// AUTHENTICATE
+  app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
 
-    // locally
-        // LOGIN
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
+    if ( !req.file.mimetype.startsWith( 'image/' ) ) {
+      return res.status( 422 ).json( {
+        error : 'The uploaded file must be an image'
+      } );
+    }
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+    var dimensions = sizeOf( req.file.path );
 
-        // SIGNUP
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
+    if ( ( dimensions.width < 100 ) || ( dimensions.height < 100 ) ) {
+      return res.status( 422 ).json( {
+        error : 'The image must be at least 2000 x 2000px'
+      } );
+    }
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+    return res.status( 200 ).send( req.file );
+  });
+
+  // LOGOUT
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // AUTHENTICATE
+  // LOGIN
+  app.get('/login', function(req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  // SIGNUP
+  // show the signup form
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
-
+  if (req.isAuthenticated())
+    return next();
     res.redirect('/');
 }
